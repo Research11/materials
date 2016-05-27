62de62965016d7b8f224c3074e1f9b7ca0a72414
Research11 <345303227@qq.com>
2016-05-27 14:14:27 +0800

    add SSHDEMO 工程

---
+++ SSHDemo/src/com/pb/domain/Person.java
@@ -0,0 +1,46 @@
+package com.pb.domain;
+
+import java.util.Date;
+
+public class Person {
+	Integer id;
+	String name;
+	Integer age;
+	String email;
+	Date birthday;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	
+	
+}
---
+++ SSHDemo/src/com/pb/domain/Users.java
@@ -0,0 +1,43 @@
+package com.pb.domain;
+
+public class Users {
+	Integer id;
+	String name;
+	String alias;
+	String email;
+	String password;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAlias() {
+		return alias;
+	}
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+	
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
